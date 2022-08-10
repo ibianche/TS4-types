82048d8134dd515b98eb5f5d547c0cc53011df97
ibianche <inna.pomorska@gmail.com>
2022-08-10 13:04:19 +0200

    funkcja z parametrem resztowym extraFees

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,12 @@
-function calculateTax(amount, discount = 0) {
-    return (amount * 1.2) - discount;
+function calculateTax(amount, discount = 0, ...extraFees) {
+    return (amount * 1.2) - discount
+        + extraFees.reduce((total, val) => total + val, 0);
 }
 let taxValue = calculateTax(100, 0);
 console.log(`Dwa argumenty: ${taxValue}`);
 taxValue = calculateTax(100);
 console.log(`Jeden argument: ${taxValue}`);
-// taxValue = calculateTax(100, 10, 20);
-// console.log(`Trzy argumenty: ${taxValue}`);
+taxValue = calculateTax(100, 10, 20);
+console.log(`Trzy argumenty: ${taxValue}`);
+taxValue = calculateTax(100, 10, 20, 1, 30, 7);
+console.log(`Sześć argumentów: ${taxValue}`);
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 
 
 
-function calculateTax(amount, discount = 0) {
-    return (amount * 1.2) - discount;
+function calculateTax(amount, discount = 0, ...extraFees) {           //extrafees do uzycia wiecej param niz 3
+    return (amount * 1.2) - discount
+    + extraFees.reduce((total, val) => total + val, 0);
 }
 let taxValue = calculateTax(100, 0);
 console.log(`Dwa argumenty: ${taxValue}`);
 taxValue = calculateTax(100);
 console.log(`Jeden argument: ${taxValue}`);
-// taxValue = calculateTax(100, 10, 20);
-// console.log(`Trzy argumenty: ${taxValue}`);
+taxValue = calculateTax(100, 10, 20);
+console.log(`Trzy argumenty: ${taxValue}`);
+taxValue = calculateTax(100,10, 20, 1, 30, 7);
+console.log(`Sześć argumentów: ${taxValue}`);
