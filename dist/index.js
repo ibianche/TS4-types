c9ac2a88397b0edac138fce8dd3b872df076b451
ibianche <inna.pomorska@gmail.com>
2022-08-10 11:54:55 +0200

    uzywanie null !==

--- dist/index.js
+++ dist/index.js
@@ -1,25 +1,35 @@
 function calculateTax(amount, format) {
+    if (amount === 0) {
+        return null;
+    }
     const calcAmount = amount * 1.2;
     return format ? `${calcAmount.toFixed(2)} zł` : calcAmount;
 }
-let taxValue = calculateTax(100, false);
-switch (typeof taxValue) {
-    case "number":
-        console.log(`Wartośc typu number: ${taxValue.toFixed(2)}`);
-        break;
-    case "string":
-        console.log(`Wartośc typu string: ${taxValue.charAt(7)}${taxValue.charAt(8)}`);
-        break;
-    default:
-        let value = taxValue;
-        console.log(`Nieoczekiwany typ dla wartości: ${value}`);
+let taxValue;
+eval("taxValue = calculateTax(100, false)");
+if (taxValue !== null) {
+    let nonNullTaxValue = taxValue;
+    switch (typeof taxValue) {
+        case "number":
+            console.log(`Wartośc typu number: ${taxValue.toFixed(2)}`);
+            break;
+        case "string":
+            console.log(`Wartośc typu string: ${taxValue.charAt(7)}${taxValue.charAt(8)}`);
+            break;
+    }
 }
-let newResult = calculateTax(200, false);
-let myNumber = newResult;
-console.log(`Wartość liczbowa: ${myNumber.toFixed(2)}`);
-//       to samo co wyzej tylko sprawdza typ wartosci w case
-// if(typeof taxValue === "number"){
-//     console.log(`Wartośc typu number: ${taxValue.toFixed(2)}`);
-// } else if(typeof taxValue === "string"){
-//     console.log(`Wartośc typu string: ${taxValue.charAt(7)}${taxValue.charAt(8)}`);
+else {
+    console.log("To nie jest wartosc typu string lub number");
+}
+// default:
+//     if(taxValue === null){
+//         console.log(`Wartość to null`);
+//     } else {
+//         console.log(typeof taxValue);
+//         let value: never = taxValue;
+//         console.log(`Nieoczekiwany typ dla wartości: ${value}`);
+//     }
 // }
+// let newResult: unknown = calculateTax(200, false);
+// let myNumber: number = newResult as number;
+// console.log(`Wartość liczbowa: ${myNumber.toFixed(2)}`);
--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,38 @@
-function calculateTax(amount: number, format: boolean): string | number {
+function calculateTax(amount: number, format: boolean): string | number | null {
+    if (amount === 0) {
+        return null;
+    }
     const calcAmount = amount * 1.2;
     return format ? `${calcAmount.toFixed(2)} zł` : calcAmount;
 }
 
-let taxValue = calculateTax(100, false);
-
-switch (typeof taxValue) {
-    case "number":
-        console.log(`Wartośc typu number: ${taxValue.toFixed(2)}`);
-        break;
-    case "string":
-        console.log(`Wartośc typu string: ${taxValue.charAt(7)}${taxValue.charAt(8)}`);
-        break;
-    default:
-        let value: never = taxValue;
-        console.log(`Nieoczekiwany typ dla wartości: ${value}`);
+let taxValue!: string | number | null;
+eval("taxValue = calculateTax(100, false)");
+if (taxValue !== null) {
+    let nonNullTaxValue: string | number = taxValue;
+    switch (typeof taxValue) {
+        case "number":
+            console.log(`Wartośc typu number: ${taxValue.toFixed(2)}`);
+            break;
+        case "string":
+            console.log(`Wartośc typu string: ${taxValue.charAt(7)}${taxValue.charAt(8)}`);
+            break;
+    }
+} else {
+    console.log("To nie jest wartosc typu string lub number");
 }
 
-let newResult: unknown = calculateTax(200, false);
-let myNumber: number = newResult as number;
-console.log(`Wartość liczbowa: ${myNumber.toFixed(2)}`);
-
-
-
-
-
-//       to samo co wyzej tylko sprawdza typ wartosci w case
-// if(typeof taxValue === "number"){
-//     console.log(`Wartośc typu number: ${taxValue.toFixed(2)}`);
-// } else if(typeof taxValue === "string"){
-//     console.log(`Wartośc typu string: ${taxValue.charAt(7)}${taxValue.charAt(8)}`);
+// default:
+//     if(taxValue === null){
+//         console.log(`Wartość to null`);
+//     } else {
+//         console.log(typeof taxValue);
+//         let value: never = taxValue;
+//         console.log(`Nieoczekiwany typ dla wartości: ${value}`);
+//     }
 // }
+
+// let newResult: unknown = calculateTax(200, false);
+// let myNumber: number = newResult as number;
+// console.log(`Wartość liczbowa: ${myNumber.toFixed(2)}`);
 
